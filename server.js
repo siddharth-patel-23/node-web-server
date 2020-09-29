eefbf77bdf4276123545513b09717664b0af4cd2
codeItSidz <sidz23999@gmail.com>
2020-09-29 13:50:03 +0530

    Setup Heroku App

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+var port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -38,6 +40,6 @@ app.get('/about', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('HTTP server started!');
 });
